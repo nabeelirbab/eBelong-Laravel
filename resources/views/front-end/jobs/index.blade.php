4fced9eb093b3d341ed5f0431cd5fdc74f43a6f4
Faizan <faizanexpertize@gmail.com>
2021-05-19 05:15:06 +0500

    fix title, and search view filter showing on all pages

--- resources/views/front-end/jobs/index.blade.php
+++ resources/views/front-end/jobs/index.blade.php
@@ -12,7 +12,7 @@
                     <div class="col-xs-12 col-sm-12 col-md-8 push-md-2 col-lg-6 push-lg-3">
                         <div class="wt-innerbannercontent">
                             <div class="wt-title">
-                                <h2>{{ trans('lang.jobs') }}</h2>
+                                <h2>Jobs</h2>
                             </div>
                             @if (!empty($show_breadcrumbs) && $show_breadcrumbs === 'true')
                                 @if (count($breadcrumbs))
