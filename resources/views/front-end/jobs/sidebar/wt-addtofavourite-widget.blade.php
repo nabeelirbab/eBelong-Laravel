e54d37bbfcba227990074e79f4b08eaefd4ac0dd
Faizan <faizanexpertize@gmail.com>
2021-05-24 22:14:14 +0500

    fix issues on asana

--- resources/views/front-end/jobs/sidebar/wt-addtofavourite-widget.blade.php
+++ resources/views/front-end/jobs/sidebar/wt-addtofavourite-widget.blade.php
@@ -1,15 +1,15 @@
 <div class="wt-clicksavearea">
     <span>{{{trans("lang.project_id")}}}: {{{$job->code}}}</span>
-    @if (!empty($save_jobs))
+    @if (!empty($user->profile->saved_jobs) && in_array($job->id, unserialize($user->profile->saved_jobs)))
         <a href="javascrip:void(0);" class="wt-clicksavebtn wt-clicksave wt-btndisbaled">
-            <i class="fa fa-heart"></i> 
+            <i class="fa fa-heart"></i>
             {{{trans("lang.saved")}}}
         </a>
     @else
         <div class="wt-clicksavearea">
-            <a href="javascript:void(0);" id="profile-{{$job->id}}" v-bind:class="disable_btn" class="wt-clicksavebtn" @click.prevent="add_wishlist('profile-{{$job->id}}', {{ $job->id }}, 'saved_jobs', '{{{trans("lang.saved")}}}')" v-cloak>
-                <i v-bind:class="heart_class"></i> 
-                @{{text}}
+            <a href="javascrip:void(0);" class="wt-clicklike" id="job-{{$job->id}}" @click.prevent="add_wishlist('job-{{$job->id}}', {{$job->id}}, 'saved_jobs', '{{trans("lang.saved")}}')" v-cloak>
+                <i class="fa fa-heart"></i>
+                <span class="save_text">{{ trans('lang.click_to_save') }}</span>
             </a>
         </div>
     @endif
