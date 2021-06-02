2145cfbe737e5e797f9ac6f5d90ef5b4867f9349
dev-shaheer97 <engr.shaheer97@gmail.com>
2021-06-02 10:57:08 -0700

    gtag script added -home

--- resources/views/homeV5.blade.php
+++ resources/views/homeV5.blade.php
@@ -81,6 +81,15 @@
 			var Map_key = {!! json_encode(Helper::getGoogleMapApiKey()) !!}
 			var APP_DIRECTION = {!! json_encode(Helper::getTextDirection()) !!}
 		</script>
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-47887669-1"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'UA-47887669-1');
+		</script>
 	</head>
 
 	<body class="wt-login lang-en ltr ">
