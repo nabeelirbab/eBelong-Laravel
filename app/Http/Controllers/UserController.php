9666fe5fd74cdd99e7082a030a567b55446dfe62
“Sabrina-tahir” <sabrinatahir7@gmail.com>
2022-02-09 21:57:13 +0500

    redundant invites in agency pt2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -530,7 +530,13 @@ class UserController extends Controller
 
         if(!empty($request->member_role)){
             $user_id = User::select('id')->where('email', $request->invitation_email)->get();
+            $alreadyInvited = DB::table('agency_associated_users')->where('agency_id', $request->agency_id)->where('user_id',$user_id[0]['id'])->get();
+            // dd($alreadyInvited);
+            if(!empty($alreadyInvited[0])){
+                Session::flash('error', 'Invitation has already sent to this user.');
+                    return Redirect::back();
 
+            }
             if (count($user_id) > 0) {
 
 
