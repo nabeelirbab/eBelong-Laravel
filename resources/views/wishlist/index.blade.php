f2b7bda64b6cf0f954e6823f45a0cb25676ca6dd
“Sabrina-tahir” <sabrinatahir7@gmail.com>
2022-03-07 22:43:55 +0500

    header and footer for wishlist

--- resources/views/wishlist/index.blade.php
+++ resources/views/wishlist/index.blade.php
@@ -1,1 +1,7 @@
+@extends(file_exists(resource_path('views/extend/front-end/master.blade.php')) ? 
+'extend.front-end.master':
+ 'front-end.master', ['body_class' => 'wt-innerbgcolor'] )
+@section('content')
 <h1> WishList </h1>
+</section>
+@endsection
