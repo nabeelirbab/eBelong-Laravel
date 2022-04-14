58268b2ccac1df1e9a6b47fe641d19c894ab1cd9
“Sabrina-tahir” <sabrinatahir7@gmail.com>
2022-04-14 16:55:13 +0500

    Sitemap Update for Talent urls

--- app/Http/Controllers/SiteMapController.php
+++ app/Http/Controllers/SiteMapController.php
@@ -8,13 +8,23 @@ use Carbon\Carbon;
 use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\SitemapGenerator;
 use Spatie\Sitemap\Tags\Url;
+use App\Category;
+use App\Skill;
 
 class SiteMapController extends Controller
 {
     public function index(){
         $path = public_path();
         $file = '/sitemap.xml';
-        SitemapGenerator::create(env('APP_URL'))
+        $sitemap = SitemapGenerator::create(env('APP_URL'))
+        ->getSitemap();
+        Category::all()->each(function (Category $Item) use ($sitemap) {
+            $sitemap->add(Url::create("/hire/{$Item->slug}"));
+        });
+        Skill::all()->each(function (Skill $Item) use ($sitemap) {
+            $sitemap->add(Url::create("/hire/{$Item->slug}"));
+        });
+        $sitemap->add(Url::create('/hire'))
         ->writeToFile($path.$file);
         return redirect(env('APP_URL')."/sitemap.xml");
     }
