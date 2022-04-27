c6eb644d56c0ae8a59bab7d1290f06152e8b7994
“Sabrina-tahir” <sabrinatahir7@gmail.com>
2022-04-27 20:22:30 +0500

    sitemap duplication issue fixed

--- app/Console/Commands/SitemapCommand.php
+++ app/Console/Commands/SitemapCommand.php
@@ -3,11 +3,19 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Http\Request;
+use App\Helper;
+use Carbon\Carbon;
 use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\SitemapGenerator;
 use Spatie\Sitemap\Tags\Url;
 use App\Category;
 use App\Skill;
+use App\Blog;
+use App\User;
+use App\Service;
+use App\Cource;
+use App\Job;
 
 class SitemapCommand extends Command
 {
@@ -45,16 +53,82 @@ class SitemapCommand extends Command
         // log('generating Sitemap');
         $path = public_path();
         $file = '/sitemap.xml';
-        $sitemap = SitemapGenerator::create(env('APP_URL'))
-        ->getSitemap();
+        $sitemap = Sitemap::create(); 
+        $sitemap->add(Url::create("/hire"));
+        $sitemap->add(Url::create("/blogs"));
+        $sitemap->add(Url::create("/courses"));
+        $sitemap->add(Url::create("/"));
+        $sitemap->add(Url::create("/jobs"));
+        $sitemap->add(Url::create("/services"));
         Category::all()->each(function (Category $Item) use ($sitemap) {
             $sitemap->add(Url::create("/hire/{$Item->slug}"));
         });
         Skill::all()->each(function (Skill $Item) use ($sitemap) {
+            if($Item->title=='E-Commerce'||$Item->title=='Cloud Computing'||$Item->title=='Data Science'||
+            $Item->title=='Graphic Designing'||$Item->title=='Artificial Intelligence'||$Item->title=='Growth Hacking'){
+                
+            }
+            else{
             $sitemap->add(Url::create("/hire/{$Item->slug}"));
+            }
         });
-        $sitemap->add(Url::create('/hire'))
-        ->writeToFile($path.$file);
+        User::all()->each(function (User $Item) use ($sitemap) {
+            $sitemap->add(Url::create("/profile/{$Item->slug}"));
+        });
+        Category::all()->each(function (Category $Item) use ($sitemap) {
+            $sitemap->add(Url::create("/services/{$Item->slug}"));
+        });
+        Skill::all()->each(function (Skill $Item) use ($sitemap) {
+            if($Item->title=='E-Commerce'||$Item->title=='Cloud Computing'||$Item->title=='Data Science'||
+            $Item->title=='Graphic Designing'||$Item->title=='Artificial Intelligence'||$Item->title=='Growth Hacking'){
+                
+            }
+            else{
+            $sitemap->add(Url::create("/services/{$Item->slug}"));
+            }
+        });
+        Service::all()->each(function (Service $Item) use ($sitemap) {
+            $sitemap->add(Url::create("/service/{$Item->slug}"));
+        });
+        Category::all()->each(function (Category $Item) use ($sitemap) {
+            $sitemap->add(Url::create("/jobs/{$Item->slug}"));
+        });
+        Job::all()->each(function (Job $Item) use ($sitemap) {
+            $sitemap->add(Url::create("/job/{$Item->slug}"));
+        });
+        Skill::all()->each(function (Skill $Item) use ($sitemap) {
+            if($Item->title=='E-Commerce'||$Item->title=='Cloud Computing'||$Item->title=='Data Science'||
+            $Item->title=='Graphic Designing'||$Item->title=='Artificial Intelligence'||$Item->title=='Growth Hacking'){
+                
+            }
+            else{
+            $sitemap->add(Url::create("/jobs/{$Item->slug}"));
+            }
+        });
+        Category::all()->each(function (Category $Item) use ($sitemap) {
+            $sitemap->add(Url::create("/courses/{$Item->slug}"));
+        });
+        Skill::all()->each(function (Skill $Item) use ($sitemap) {
+            if($Item->title=='E-Commerce'||$Item->title=='Cloud Computing'||$Item->title=='Data Science'||
+            $Item->title=='Graphic Designing'||$Item->title=='Artificial Intelligence'||$Item->title=='Growth Hacking'){
+                
+            }
+            else{
+            $sitemap->add(Url::create("/courses/{$Item->slug}"));
+            }
+        });
+        Cource::all()->each(function (Cource $Item) use ($sitemap) {
+            $sitemap->add(Url::create("/course/{$Item->slug}"));
+        });
+        Blog::all()->each(function (Blog $Item) use ($sitemap) {
+            $sitemap->add(Url::create("/blog/{$Item->slug}"));
+        });
+        
+        $sitemap->add(Url::create("page/about-us"));
+        $sitemap->add(Url::create("page/privacy-policy"));
+        $sitemap->add(Url::create("/"));
+
+        $sitemap->writeToFile($path.$file);
         
     }
 }
