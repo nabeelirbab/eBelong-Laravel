98c2d5d5f814024a3e0961521bd2f39e2ba36dbc
Sabrina-tahir <57313042+Sabrina-tahir@users.noreply.github.com>
2022-05-10 18:44:50 +0500

    Revert "category logos for admin"

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -13,27 +13,27 @@
             <?php if($user_role == 'guest'||$user_role == 'editor'): ?>
             <li style="order: 1;">
                 <a href="{{url('/courses')}}">
-                    {{{ trans('lang.courses') }}}
+                    {{{ trans('lang.browse_instructors') }}}
                 </a>
             </li>
                 <li style="order: 2;">
                 <a href="{{url('hire')}}">
-                    {{{ trans('lang.Talent') }}}
+                    {{{ trans('lang.view_freelancers') }}}
                 </a>
                 </li>
                 <li style="order: 4;">
                 <a href="{{url('/jobs')}}">
-                    {{{ trans('lang.jobs') }}}
+                    {{{ trans('lang.browse_jobs') }}}
                 </a>
                 </li>
                 <li style="order: 5;">
                 <a href="{{url('/services')}}">
-                    {{{ trans('lang.services') }}}
+                    {{{ trans('lang.browse_services') }}}
                 </a>
                 </li>
                 <li style="order: 5;">
                     <a href="{{url('/blogs')}}">
-                        {{{ trans('lang.Blogs') }}}
+                        {{{ trans('lang.browse_blogs') }}}
                     </a>
                 </li>
                 <?php elseif($user_role == 'editor'): ?>
@@ -46,27 +46,27 @@
             <?php elseif($user_role == 'admin'): ?>
             <li style="order: 1;">
                 <a href="{{url('/courses')}}">
-                    {{{ trans('lang.courses') }}}
+                    {{{ trans('lang.browse_instructors') }}}
                 </a>
             </li>
                 <li style="order: 2;">
                 <a href="{{url('hire')}}">
-                    {{{ trans('lang.Talent') }}}
+                    {{{ trans('lang.view_freelancers') }}}
                 </a>
                 </li>
                 <li style="order: 4;">
                 <a href="{{url('/jobs')}}">
-                    {{{ trans('lang.jobs') }}}
+                    {{{ trans('lang.browse_jobs') }}}
                 </a>
                 </li>
                 <li style="order: 5;">
                 <a href="{{url('/services')}}">
-                    {{{ trans('lang.services') }}}
+                    {{{ trans('lang.browse_services') }}}
                 </a>
                 </li>
                 <li style="order: 6;">
                     <a href="{{url('/blogs')}}">
-                        {{{ trans('lang.Blogs') }}}
+                        {{{ trans('lang.browse_blogs') }}}
                     </a>
                 </li>
             <?php elseif($user_role == 'employer'): ?>
@@ -86,12 +86,12 @@
             <?php elseif($user_role == 'freelancer'): ?>
             <li style="order: 1;">
                 <a href="{{url('/courses')}}">
-                    {{{ trans('lang.courses') }}}
+                    {{{ trans('lang.browse_instructors') }}}
                 </a>
             </li>
                 <li style="order: 4;">
                     <a href="{{url('search-results?type=freelancerjobs')}}">
-                    {{{ trans('lang.jobs') }}}
+                    {{{ trans('lang.browse_jobs') }}}
                 </a>
                 </li>
                 
