dc339037547ea64b75fc91acf25d3f2ff57b378c
“Sabrina-tahir” <sabrinatahir7@gmail.com>
2022-05-12 01:32:35 +0500

    fix wrong redirect of tabs

--- resources/views/back-end/admin/profile-settings/tabs.blade.php
+++ resources/views/back-end/admin/profile-settings/tabs.blade.php
@@ -1,7 +1,7 @@
 <div class="wt-dashboardtabs">
     <ul class="wt-tabstitle nav navbar-nav">
         <li class="nav-item">
-            <a class="{{{ \Request::route()->getName()==='personalDetail'? 'active': '' }}}" href="{{{ route('adminPersonalDetail') }}}">{{{ trans('lang.admin_detail') }}}</a>
+            <a class="{{{ \Request::route()->getName()==='personalDetail'? 'active': '' }}}" href="{{{ route('adminProfile') }}}">{{{ trans('lang.admin_detail') }}}</a>
         </li>
     </ul>
 </div>
