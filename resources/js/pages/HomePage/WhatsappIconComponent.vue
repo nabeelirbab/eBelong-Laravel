fe83e5d93ebf87ea9f070ff20ad78078951e777e
Hanzila javeed <hani.javeed@foodservicedirect.com>
2022-05-12 21:41:46 +0500

    add whastapp icon for messages

---
+++ resources/js/pages/HomePage/WhatsappIconComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="whatsapp-icon">
+    <!-- <img src="{{ asset('uploads/settings/general/whatsapp-logo.png') }}" alt="user-img"> -->
+    <a
+      href="https://www.freepnglogos.com/pics/whatsapp-logo-png"
+      target="_blank"
+      title="whatsapp icon"
+    >
+      <img
+        :src="`/uploads/settings/general/whatsapp-logo.png`"
+        alt="whatsapp"
+      />
+    </a>
+  </div>
+</template>
+<!-- v-if="i < 4 && item.is_featured" -->
+<script>
+export default {
+  name: "WhatsappIconComponent",
+};
+</script>
---
+++ resources/views/front-end/includes/whatsapp.blade.php
@@ -0,0 +1,5 @@
+<div class="whatsapp-icon">
+    <!-- <img src="{{ asset('uploads/settings/general/whatsapp-logo.png') }}" alt="user-img"> -->
+	<a href="https://www.freepnglogos.com/pics/whatsapp-logo-png" target="_blank" title="whatsapp icon"><img src="{{ asset('uploads/settings/general/whatsapp-logo.png') }}" alt="whatsapp" /></a>
+
+</div>
