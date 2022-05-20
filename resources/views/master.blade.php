b31872be9ef29566441fa0d263c36db9066d128b
“Sabrina-tahir” <sabrinatahir7@gmail.com>
2022-05-20 19:02:49 +0500

    social share check

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -19,6 +19,7 @@
 	<meta name="keywords" content="@yield('keywords')">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:image" content="https://ebelongmaster-1517a.kxcdn.com/uploads/services/873/medium-1629458797-download%20(12).jpg">
 	<link rel="apple-touch-icon" href="apple-touch-icon.png">
 	<link rel="icon" href="{{{ asset(Helper::getSiteFavicon()) }}}" type="image/x-icon">
 	<link href="{{asset('css/app.css') }}" rel="stylesheet">
--- routes/web.php
+++ routes/web.php
@@ -355,8 +355,8 @@ Route::group(
             'as' => 'UserData', 
             'uses' => 'UserController@getLogNotificationData'
         ]);
-        Route::get('/admin/send-notifications','UserController@viewNotificationData')->name('viewMobileNotification');
-        Route::post('/admin/send-notifications-post','UserController@sendNotificationData')->name('sendMobileNotification');
+        // Route::get('/admin/send-notifications','UserController@viewNotificationData')->name('viewMobileNotification');
+        // Route::post('/admin/send-notifications-post','UserController@sendNotificationData')->name('sendMobileNotification');
         Route::post('/admin/login-notification-updated','UserController@updateNotificationData');
 		
 
@@ -515,6 +515,8 @@ Route::group(
         Route::post('course/get-course-settings', 'CourseController@getCourseSettings');
         Route::post('service/update-service', 'ServiceController@update');
         Route::post('service/update-course', 'CourseController@update');
+        Route::get('/send-notifications','UserController@viewNotificationData')->name('viewMobileNotification');
+        Route::post('/send-notifications-post','UserController@sendNotificationData')->name('sendMobileNotification');
     }
 );
 //Employer Routes
