4db21c8b4693f753d2578f9818546914c3e9d20f
“Sabrina-tahir” <sabrinatahir7@gmail.com>
2022-06-24 17:53:37 +0500

    inner-page-edit in menus

--- resources/views/back-end/includes/dashboard-menu.blade.php
+++ resources/views/back-end/includes/dashboard-menu.blade.php
@@ -230,6 +230,7 @@
                                 <li><hr><a href="{{{ url(route('editorProfile'))}}}">{{ trans('lang.acc_settings') }}</a></li>
                                 {{-- <li><hr><a href="{{{ url('admin/settings') }}}">{{ trans('lang.general_settings') }}</a></li> --}}
                                 <li><hr><a href="{{{ route('resetPassword') }}}">{{ trans('lang.reset_pass') }}</a></li>
+                                <li><hr><a href="{{{ url('editor/settings/inner-pages') }}}">{{ "Inner Pages" }}</a></li>
                             </ul>
                         </li>
                         <li class="menu-item-has-children">
