43247533311f0848de38f30a6a35f5a227f9eaab
“Sabrina-tahir” <sabrinatahir7@gmail.com>
2022-07-04 22:32:37 +0500

    job debugging thru changing id to services instead of jobs

--- resources/views/front-end/jobs/jobListing.blade.php
+++ resources/views/front-end/jobs/jobListing.blade.php
@@ -36,7 +36,7 @@
             </div>
         </div>
     @endif
-    <div id="jobsTest">
+    <div id="services">
     <div class="wt-haslayout wt-main-section" >
     <div class="search-form">
               <search-form
